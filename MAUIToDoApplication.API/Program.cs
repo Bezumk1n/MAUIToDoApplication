a660e88995f6a7f59ed87a8c1b0116f9767e0269
Umnov Stanislav <umnov.msk@gmail.com>
2023-09-19 22:19:39 +0300

    Added api endpoints: GET, POST, PUT, DELETE

--- MAUIToDoApplication.API/Program.cs
+++ MAUIToDoApplication.API/Program.cs
@@ -1,4 +1,5 @@
 using MAUIToDoApplication.API.Data;
+using MAUIToDoApplication.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace MAUIToDoApplication.API
@@ -11,9 +12,37 @@ namespace MAUIToDoApplication.API
 
             builder.Services.AddDbContext<AppDbContext>(opt => 
                 opt.UseNpgsql(builder.Configuration.GetConnectionString("NpgSqlConnection")));
-
             var app = builder.Build();
-            //app.UseHttpsRedirection();
+
+            app.MapGet("api/todo", async (AppDbContext context) =>
+            { 
+                var items = await context.ToDos.ToListAsync();
+                return Results.Ok(items);
+            });
+            app.MapPost("api/todo", async (AppDbContext context, ToDo todo) =>
+            {
+                await context.ToDos.AddAsync(todo);
+                await context.SaveChangesAsync();
+                return Results.Created($"api/todo/{todo.Id}", todo);
+            });
+            app.MapPut("api/todo/{id}", async (AppDbContext context, Guid id, ToDo toDo) =>
+            {
+                var toDoModel = await context.ToDos.FirstOrDefaultAsync(q => q.Id == id);
+                if(toDoModel == null)
+                    return Results.NotFound();
+                toDoModel.ToDoName = toDo.ToDoName;
+                await context.SaveChangesAsync();
+                return Results.NoContent();
+            });
+            app.MapDelete("api/todo/{id}", async (AppDbContext context, Guid id) =>
+            {
+                var toDoModel = await context.ToDos.FirstOrDefaultAsync(q => q.Id == id);
+                if (toDoModel == null)
+                    return Results.NotFound(id);
+                context.ToDos.Remove(toDoModel);
+                await context.SaveChangesAsync();
+                return Results.NoContent();
+            });
             app.Run();
         }
     }
