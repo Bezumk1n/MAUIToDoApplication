8c9b5ab98993fd33872a58b1ca1f643b1e7ef0b6
Umnov Stanislav <umnov.msk@gmail.com>
2023-10-14 22:46:43 +0300

    Add DelegateCommand class

---
+++ MAUIToDoApplication.Client/Common/DelegateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MAUIToDoApplication.Client.Common
+{
+    public class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action<object> _execute;
+        private Func<bool> _predicate;
+        public DelegateCommand(Action<object> executeMethod, Func<bool> predicate = null)
+        {
+            _execute = executeMethod;
+            _predicate = predicate;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            if (_predicate == null)
+                return true;
+            return _predicate.Invoke();
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+}
