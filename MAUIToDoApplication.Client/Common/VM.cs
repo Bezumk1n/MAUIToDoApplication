2acb401dd36eea4fff2d509650d4bcfaaf457efe
Umnov Stanislav <umnov.msk@gmail.com>
2023-10-14 22:46:10 +0300

    Added base ViewModel class

---
+++ MAUIToDoApplication.Client/Common/VM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUIToDoApplication.Client.Common
+{
+    internal class VM : NotifyObject
+    {
+        /// <summary>
+        /// Title of the ViewModel
+        /// </summary>
+        public virtual string Title { get; set; }
+    }
+}
